2db15d630d7aa68e920d0b675ff528ea70535c3f
AyushGoyal007 <97087970+AyushGoyal007@users.noreply.github.com>
2022-01-21 17:04:58 +0530

    Assignment 2 work

---
+++ assignment 2 output file.py
@@ -0,0 +1,36 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+======= RESTART: C:\Users\HP\OneDrive\Desktop\assignment 2 source code.py ======
+36
+.egaugnal evitisnes esac a si nohtyP
+ case sensitive
+Python is object oriented language.
+10
+Pythonisacasesensitivelanguage. 
+
+What is your name:ayush goyal
+What is your SID:21105020
+What is your department:ece
+What is your CGPA:9.9
+Hey ayush goyal Here!
+My SID is 21105020
+I am from  ece department and my CGPA is 9.9 
+
+a&b is: 8
+a|b is: 58
+a^b is: 50
+a<<2 : 224 	 b<<2: 40
+a>>2 : 14 	 b>>4: 0 
+
+enter a:21
+enter b:56
+enter c:30
+b is the greatest number 
+
+yes 
+
+length of first side is:12
+length of second side is:2
+length of third side is:7
+No
